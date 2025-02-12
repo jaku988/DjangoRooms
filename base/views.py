2a2445a71641ef016fef997df88c0bdfaf987446
= <=>
2025-02-12 14:39:38 +0100

    Register page and logic created, upgraded login and info in navbar

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 def login_page(request):
     page = 'login'
@@ -17,6 +18,8 @@ def login_page(request):
             if user is not None:
                 login(request, user)
                 return redirect("home")
+            else:
+                err_message = "Incorrect username or password!"
 
     context = {
         'page': page,
@@ -28,14 +31,28 @@ def login_page(request):
 def register_page(request):
     page = 'register'
 
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
     context = {
         'page': page,
+        'form' : form,
     }
     return render(request, 'base/login_page.html', context)
 
 @login_required(login_url='login_page')
 def logout_page(request):
-    pass
+    logout(request)
+    return redirect("home")
 
 
 #strona startowa aplikacji
