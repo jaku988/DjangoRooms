4b60db02adbeaf95f069df84bf75847762d33a55
= <=>
2025-02-12 13:42:01 +0100

    Login page and logic created

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login-page', views.login_page, name='login'),
+    path('register-page', views.register_page, name='register'),
+    path('logout', views.logout_page, name='logout'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+def login_page(request):
+    page = 'login'
+    err_message = ''
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(username=username).exists():
+            err_message = "User does not exist!"
+        else:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+
+    context = {
+        'page': page,
+        'err_message': err_message,
+    }
+    return render(request, 'base/login_page.html', context)
 
 
+def register_page(request):
+    page = 'register'
+
+    context = {
+        'page': page,
+    }
+    return render(request, 'base/login_page.html', context)
+
+@login_required(login_url='login_page')
+def logout_page(request):
+    pass
 
 
 #strona startowa aplikacji
